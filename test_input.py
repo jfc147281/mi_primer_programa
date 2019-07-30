82f9924008a175fe013784fec359bb1fb7b28fd0
juan_ <juan_franciscocalde@hotmail.com>
2019-07-29 22:09:43 -0300

    probando

---
+++ test_input.py
@@ -0,0 +1,9 @@
+numero_del_usuario = input("Dime un numero: ")
+print("el numero del usuario es :", numero_del_usuario)
+
+primer_numero = int(input("primer_numero: "))
+segundo_numero = int(input("segundo numero: "))
+tercer_numero = int(input("tercer_numero: "))
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+print("el mayor numero ingresado es: ",resultado)
+
